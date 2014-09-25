a5524481f435828954ca626f5f11d04482ea3a80
Altti Tammi <altti.tammi@gmail.com>
2014-09-25 12:05:19 +0300

    Windows installer + Readme

---
+++ README.md
@@ -0,0 +1,10 @@
+mc-mapfix
+=======================
+Fix for map markers disappearing in version 1.8
+
+Linux and OSX users: use mapfix_installer.sh (bash)
+Windows users use:   mapfix_installer.ps1 (PowerShell)
+
+Windows users might run into Execution policy errors. In this case launch the script with
+
+powershell.exe -ExecutionPolicy Unrestricted -File "c:\mapfix_installer.ps1" 
---
+++ mapfix_installer.ps1
@@ -0,0 +1,35 @@
+$version="1.8"
+$offset=0x923
+$newvalue=0xFF
+$classfile="bue.class"
+$postfix="_mapfix"
+
+Set-Location -Path $env:APPDATA\.minecraft\versions
+
+if (Test-Path $version) {
+  if (Test-Path $version$postfix)
+  {
+    Write-Host Folder $version$postfix already exists!
+  }
+  else
+  {
+    New-Item $version$postfix -type directory
+    Copy-Item $version\* $version$postfix -Recurse
+    Set-Location -Path $version$postfix
+    & "jar" xf ($version + ".jar") $classfile
+    $bytes = [System.IO.File]::ReadAllBytes($env:APPDATA + "\.minecraft\versions\" + $version + $postfix + "\" + $classfile);
+    $bytes[$offset] = $newvalue;
+    [System.IO.File]::WriteAllBytes($env:APPDATA + "\.minecraft\versions\" + $version + $postfix + "\" + $classfile, $bytes);
+    Move-Item $version".jar" $version$postfix".jar"
+    jar Muf $version$postfix".jar" $classfile
+    Remove-Item $classfile
+    $in='^\s*"id":\s*"' + $version + '",.*$'
+    $out='  "id": "' + $version + $postfix + '",'
+    (Get-Content $version".json") -replace $in, $out | Set-Content $version$postfix".json"
+    Remove-Item $version".json"
+  }
+}
+else
+{
+  Write-Host Folder $version not found
+}
